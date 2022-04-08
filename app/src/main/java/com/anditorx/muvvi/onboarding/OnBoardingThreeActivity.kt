602a160125cfdb04ba162f4690e6d54dd4f0e18a
Andi Rustianto <anditodev46@gmail.com>
2022-04-09 06:35:49 +0700

    onboarding

--- app/src/main/java/com/anditorx/muvvi/onboarding/OnBoardingThreeActivity.kt
+++ app/src/main/java/com/anditorx/muvvi/onboarding/OnBoardingThreeActivity.kt
@@ -1,12 +1,22 @@
 package com.anditorx.muvvi.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.anditorx.muvvi.R
+import com.anditorx.muvvi.sign.SignInActivity
+import kotlinx.android.synthetic.main.activity_on_boarding_one.*
 
 class OnBoardingThreeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding_three)
+
+        btn_next.setOnClickListener {
+            val intent = Intent(this@OnBoardingThreeActivity,
+                SignInActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/anditorx/muvvi/onboarding/OnBoardingTwoActivity.kt
+++ app/src/main/java/com/anditorx/muvvi/onboarding/OnBoardingTwoActivity.kt
@@ -1,12 +1,27 @@
 package com.anditorx.muvvi.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.anditorx.muvvi.R
+import com.anditorx.muvvi.sign.SignInActivity
+import kotlinx.android.synthetic.main.activity_on_boarding_one.*
 
 class OnBoardingTwoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding_two)
+
+        btn_next.setOnClickListener {
+            val intent = Intent(this@OnBoardingTwoActivity,
+                OnBoardingThreeActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_skip.setOnClickListener {
+            val intent = Intent(this@OnBoardingTwoActivity,
+                SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
