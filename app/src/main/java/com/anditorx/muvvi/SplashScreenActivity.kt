ad0f21c85267d0b0fb8b9b4d0dcacc740fbf19d6
Andi Rustianto <anditodev46@gmail.com>
2022-04-09 05:17:00 +0700

    handle splashscreen

--- app/src/main/java/com/anditorx/muvvi/SplashScreenActivity.kt
+++ app/src/main/java/com/anditorx/muvvi/SplashScreenActivity.kt
@@ -1,11 +1,21 @@
 package com.anditorx.muvvi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.anditorx.muvvi.onboarding.OnBoardingOneActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        var handler = Handler()
+        handler.postDelayed({
+            var intent = Intent(this@SplashScreenActivity, OnBoardingOneActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
