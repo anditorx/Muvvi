15adcc4e3d49c4c18261ce894ba18c95c95cac85
Andi Rustianto <anditodev46@gmail.com>
2022-04-09 07:16:27 +0700

    setup firebase

--- app/src/main/java/com/anditorx/muvvi/sign/SignInActivity.kt
+++ app/src/main/java/com/anditorx/muvvi/sign/SignInActivity.kt
@@ -1,12 +1,24 @@
 package com.anditorx.muvvi.sign
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.anditorx.muvvi.R
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import com.google.firebase.database.*
+import com.google.firebase.database.FirebaseDatabase
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        btn_sign_in.setOnClickListener{
+        // Write a message to the database
+            // Write a message to the database
+            val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+            val myRef: DatabaseReference = database.getReference("message")
+
+            myRef.setValue("Hello, World!")
+        }
     }
 }
